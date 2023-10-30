7d433636aecf5f588b4d2662d4ffe852ad62c24b
Maroun Nawwar <68354149+MarounNawwar@users.noreply.github.com>
2023-10-30 17:13:46 +0200

    Update ValidatorTests.java to break test #3

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("NotSoEmpty");
+		person.setFirstName("NotEmpty");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
